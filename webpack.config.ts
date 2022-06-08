8ec861f5b01df42e9ef3dbfcb92c1cad487da2bc
Dmytro Kurchenko <dmytro.kurchenko@sigma.software>
2022-06-08 15:50:14 +0300

    Add webpack configuration

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,1 +1,48 @@
-export {};
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: { path: path.join(__dirname, 'build'), filename: 'build.js' },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: true,
+    port: 5000,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' }), new ForkTsCheckerWebpackPlugin()],
+};
